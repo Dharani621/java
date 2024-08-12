3b456ca04a11270d483fcfc529d78518e7a566dc
Dharani621 <dharaniniha3@gmail.com>
2024-08-12 17:35:00 +0100

    Add files via upload

---
+++ add.java
@@ -0,0 +1,18 @@
+package firstjava;
+
+public class add {
+	public static void main(String[] args) {
+		int a=45;
+		int b=32;
+		int sum=a+b;
+		System.out.println("addition="+ sum);
+		
+		int sub=a-b;
+		System.out.println("Subtract="+sub);
+		
+		int multiplication=a*b;
+		System.out.println("Multiplication="+multiplication);
+		
+	}
+
+}
